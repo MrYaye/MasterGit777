2a87e52fc23be97eb6dec18b5b7ae3ffdcf291ab
Laboratorio302 <estudiante302@unicamacho.co>
2024-08-27 20:50:55 -0500

    MASTERGIT777

---
+++ EjercicioIF.java
@@ -0,0 +1,14 @@
+public class EjercicioIF {
+
+    float salarioSinAjuste;
+    float aumento;
+    float salarioConAjuste;
+    
+    public EjercicioIF(float salarioSinAjuste, float aumento, float salarioConAjuste){
+
+        this.salarioSinAjuste = 0;
+        this.aumento = 0;
+        this.salarioConAjuste = 0;
+
+    }
+}
