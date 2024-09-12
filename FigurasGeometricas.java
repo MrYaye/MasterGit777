5b05a46532f719fdaf0f45886b51f1cbb4277610
Laboratorio302 <estudiante302@unicamacho.co>
2024-09-11 20:26:07 -0500

    figuras

---
+++ FigurasGeometricas.java
@@ -0,0 +1,40 @@
+public class FigurasGeometricas {
+    
+float LadoCuadrado;
+float BaseTriangulo;
+float AlturaTriangulo;
+float RadioCirculo;
+int OptionFigura;
+
+
+public FigurasGeometricas(float ladoCuadrado, float baseTriangulo, float alturaTriangulo, float radioCirculo,
+        int optionFigura) {
+    this.LadoCuadrado = ladoCuadrado;
+    this.BaseTriangulo = baseTriangulo;
+    this.AlturaTriangulo = alturaTriangulo;
+    this.RadioCirculo = radioCirculo;
+    this.OptionFigura = optionFigura;
+
+
+switch (OptionFigura) {
+
+     case 1:
+
+     break;
+
+     case 2:
+
+     break;
+
+     case 3:
+
+     break;
+
+     default:
+     break;
+}
+
+}
+
+
+}
